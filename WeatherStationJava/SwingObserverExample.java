13f9775d790598db9186e438a768a9d371125f80
zuzhi <476513451@qq.com>
2018-12-21 18:14:18 +0800

    Add Swing example

---
+++ WeatherStationJava/SwingObserverExample.java
@@ -0,0 +1,44 @@
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.SwingUtilities;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class SwingObserverExample {
+    JFrame frame;
+
+    public static void main(String[] args) {
+        SwingObserverExample example = new SwingObserverExample();
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                example.go();
+            }
+        });
+    }
+
+    public void go() {
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton button = new JButton("Should I do it?");
+        button.addActionListener(new AngelListener());
+        button.addActionListener(new DevilListener());
+        frame.getContentPane().add(BorderLayout.CENTER, button);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    class AngelListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            System.out.println("Don't do it, you might regret it!");
+        }
+    }
+
+    class DevilListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            System.out.println("Come on, do it!");
+        }
+    }
+}
