1044505f78f877453fbb3f77ab5f333a9864dee9
zuzhi <476513451@qq.com>
2018-12-21 18:14:05 +0800

    Add Weather Station using Java Observable

---
+++ WeatherStationJava/WeatherData.java
@@ -0,0 +1,36 @@
+import java.util.Observable;
+import java.util.Observer;
+
+public class WeatherData extends Observable {
+
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public WeatherData() {
+    }
+
+    public float getTemperature() {
+        return this.temperature;
+    }
+
+    public float getHumidity() {
+        return this.humidity;
+    }
+
+    public float getPressure() {
+        return this.pressure;
+    }
+
+    public void measurementsChanged() {
+        setChanged();
+        notifyObservers();
+    }
+
+    public void setMeasurements(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        measurementsChanged();
+    }
+}
